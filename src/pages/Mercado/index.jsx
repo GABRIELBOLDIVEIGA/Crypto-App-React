29e1f2ad019e2bf8466bc16623f1ec5915b0946d
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-03-16 18:53:30 -0300

    Refatorando
    criando componente pra melhor legibilidade

--- src/pages/Mercado/index.jsx
+++ src/pages/Mercado/index.jsx
@@ -3,6 +3,7 @@ import Buscador from "components/Buscador";
 import Ordenador from "components/Ordenador";
 import { useState } from "react";
 import Itens from "./Itens";
+import Indicadores from "components/Indicadores";
 
 export default function Mercado() {
     const [busca, setBusca] = useState("");
@@ -16,22 +17,7 @@ export default function Mercado() {
                 <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
             </div>
 
-            <div className={styles.mercado__indicador}>
-                <div className={styles.mercado__indicador__rankName}>
-                    <p>Rank</p>
-                    <p>Nome</p>
-                </div>
-
-                <p className={styles.mercado__indicador__preco}>Preço</p>
-
-                <div className={styles.mercado__indicador__tempo}>
-                    <p>1h</p>
-                    <p>1d</p>
-                    <p>1w</p>
-                </div>
-
-                <p className={styles.mercado__indicador__grafico}>Grafico semanal</p>
-            </div>
+            <Indicadores />
 
             <Itens busca={busca} ordenador={ordenador} />
         </div>
