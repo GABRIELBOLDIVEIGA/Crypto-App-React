08d32a5b08ad076b97323a369504f8d2c4fd5fbf
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-03-16 18:55:11 -0300

    Estilizando pagina
    criando os cardes de noticias

--- src/pages/News/Card/index.jsx
+++ src/pages/News/Card/index.jsx
@@ -1,16 +1,16 @@
 import styles from "./Card.module.scss";
-import { Link } from "react-router-dom";
 
 export default function Card({ news }) {
-  return (
-    <li>
-    <Link target="_blank" to={news.link}>
-      <div className={styles.artigo}>
-        <img src={news.imgURL} alt={news.title} className={styles.news} />
-        <h1>{news.title}</h1>
-        <p>{news.description}</p>
-      </div>
-    </Link>
-    </li>
-  );
+    return (
+        <li className={styles.artigo}>
+            <a target="_blank" rel="noreferrer" href={news.link}>
+                <img src={news.imgURL} alt={news.title} className={styles.news} />
+                <div className={styles.artigo__texto}>
+                    <h1>{news.title}</h1>
+                    <p>{news.description}</p>
+                </div>
+                <p>Saiba Mais</p>
+            </a>
+        </li>
+    );
 }
--- src/pages/News/index.jsx
+++ src/pages/News/index.jsx
@@ -1,27 +1,18 @@
 import styles from "./News.module.scss";
-import { useEffect, useState } from "react";
 import Card from "./Card";
+import { useNewsContext } from 'common/context/News';
 
 export default function News() {
-  const [news, setNews] = useState([]);
-
-  useEffect(() => {
-    fetch("https://api.coinstats.app/public/v1/news/trending?skip=0&limit=20")
-      .then((response) => response.json())
-      .then((dados) => {
-        console.log(dados.news);
-        setNews(dados.news);
-      });
-  }, []);
-
-  return (
-    <section className={styles.news}>
-      <h1>News</h1>
-      <ul className={styles.news__container}>
-        {news.map((article, index) => (
-          <Card key={index} news={article} />
-        ))}
-      </ul>
-    </section>
-  );
+    const { news } = useNewsContext();
+    
+    return (
+        <section className={styles.news}>
+            <h1>News</h1>
+            <ul className={styles.news__container}>
+                {news.map((article, index) => (
+                    <Card key={index} news={article} />
+                ))}
+            </ul>
+        </section>
+    );
 }
