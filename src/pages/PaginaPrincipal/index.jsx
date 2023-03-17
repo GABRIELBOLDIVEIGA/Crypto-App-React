b7307b974c3b864a0c177d931ae88192ad85d117
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-03-16 21:34:41 -0300

    definindo tamanho maximo para o conteudo da pagina principal

--- src/pages/PaginaPrincipal/index.jsx
+++ src/pages/PaginaPrincipal/index.jsx
@@ -7,20 +7,20 @@ import MoedasAPIProvider from "common/context/MoedasAPI";
 import NewsProvider from "common/context/News";
 
 export default function PaginaPrincipal() {
-  return (
-    <div className={styles.paginaPrincipal}>
-      <Menu />
-      <div className={styles.paginaPrincipal__container}>
-        <Cabecalho />
+    return (
+        <div className={styles.paginaPrincipal}>
+            <Menu />
 
-        <NewsProvider>
-          <MoedasAPIProvider>
-            <FavoritosProvider>
-              <Outlet />
-            </FavoritosProvider>
-          </MoedasAPIProvider>
-        </NewsProvider>
-      </div>
-    </div>
-  );
+            <div className={styles.paginaPrincipal__container}>
+                <Cabecalho />
+                <NewsProvider>
+                    <MoedasAPIProvider>
+                        <FavoritosProvider>
+                            <Outlet />
+                        </FavoritosProvider>
+                    </MoedasAPIProvider>
+                </NewsProvider>
+            </div>
+        </div>
+    );
 }
