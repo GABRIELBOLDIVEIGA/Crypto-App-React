2828b8b15eb8639f480dfed1141bf25e62655a96
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-03-17 09:56:34 -0300

    Estilo do menu finalizado

--- src/components/Menu/MenuLink/index.jsx
+++ src/components/Menu/MenuLink/index.jsx
@@ -1,18 +1,80 @@
 import styles from "./MenuLink.module.scss";
+import { AiOutlineBarChart } from "react-icons/ai";
+import { AiOutlineLineChart } from "react-icons/ai";
+import { MdFavoriteBorder } from "react-icons/md";
+import { VscArrowSwap } from "react-icons/vsc";
+import { IoNewspaperOutline } from "react-icons/io5";
 import { NavLink } from "react-router-dom";
 
-export default function MenuLink({ children, to, srcImg }) {
+export default function MenuLink2({ expandido }) {
+    const iconSize = 20;
+
     return (
-        <NavLink
-            className={({ isActive }) => `
+        <div className={styles.menuLink}>
+            <NavLink
+                className={({ isActive }) => `
                 ${styles.link}
                 ${isActive ? styles.linkDestacado : ""}
+                ${expandido ? styles.expandido : ""}
             `}
-            to={to}
-            end
-        >
-            <img className={styles.img} src={srcImg} alt="" />
-            {children}
-        </NavLink>
+                to={"/mercado"}
+                end
+            >
+                <AiOutlineBarChart size={iconSize} />
+                {expandido ? <p>Mercado</p> : <></>}
+            </NavLink>
+
+            <NavLink
+                className={({ isActive }) => `
+                ${styles.link}
+                ${isActive ? styles.linkDestacado : ""}
+                ${expandido ? styles.expandido : ""}
+            `}
+                to={"/portifolio"}
+                end
+            >
+                <AiOutlineLineChart size={iconSize} />
+                {expandido ? <p>Portfolio</p> : <></>}
+            </NavLink>
+
+            <NavLink
+                className={({ isActive }) => `
+                ${styles.link}
+                ${isActive ? styles.linkDestacado : ""}
+                ${expandido ? styles.expandido : ""}
+            `}
+                to={"/favoritos"}
+                end
+            >
+                <MdFavoriteBorder size={iconSize} />
+                {expandido ? <p>Favoritos</p> : <></>}
+            </NavLink>
+
+            <NavLink
+                className={({ isActive }) => `
+                ${styles.link}
+                ${isActive ? styles.linkDestacado : ""}
+                ${expandido ? styles.expandido : ""}
+            `}
+                to={"/swap"}
+                end
+            >
+                <VscArrowSwap size={iconSize} />
+                {expandido ? <p>Swap</p> : <></>}
+            </NavLink>
+
+            <NavLink
+                className={({ isActive }) => `
+                ${styles.link}
+                ${isActive ? styles.linkDestacado : ""}
+                ${expandido ? styles.expandido : ""}
+            `}
+                to={"/news"}
+                end
+            >
+                <IoNewspaperOutline size={iconSize} />
+                {expandido ? <p>News</p> : <></>}
+            </NavLink>
+        </div>
     );
 }
--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,12 +1,8 @@
 import styles from "./Menu.module.scss";
-import MenuLink from "./MenuLink";
-import icons from "./icones.json";
-import Rodape from "components/Rodape";
-import { TfiArrowCircleLeft, TfiArrowCircleRight } from "react-icons/tfi";
-import { AiOutlineBarChart } from "react-icons/ai";
 import { useState } from "react";
-import classNames from "classnames";
-import MenuLink2 from "./MenuLink2/index";
+import { TfiArrowCircleLeft, TfiArrowCircleRight } from "react-icons/tfi";
+import MenuLink from "./MenuLink/index";
+import Rodape from "components/Rodape";
 
 export default function Menu() {
     const [expandido, setExpandido] = useState(true);
@@ -17,20 +13,16 @@ export default function Menu() {
 
     return (
         <aside className={styles.menu}>
-            
-            <nav
-                className={classNames({
-                    [styles.menu__lista]: true,
-                    [styles.expandido]: expandido,
-                })}
-            ></nav>
-
-            <div className={styles.menu__icon}>{expandido ? <TfiArrowCircleLeft size={20} onClick={handleExpandido} /> : <TfiArrowCircleRight size={20} onClick={handleExpandido} />}</div>
+            <div className={styles.menu__icon}>
+                {expandido ? 
+                    <TfiArrowCircleLeft size={20} onClick={handleExpandido} /> : 
+                    <TfiArrowCircleRight size={20} onClick={handleExpandido} />}
+            </div>
 
             <h5>MENU</h5>
 
-            <MenuLink2 expandido={expandido} />
-            
+            <MenuLink expandido={expandido} />
+
             <Rodape expandido={expandido} />
         </aside>
     );
