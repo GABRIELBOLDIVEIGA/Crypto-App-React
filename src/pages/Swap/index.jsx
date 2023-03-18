33df34e1d0cfe70ff428a8e21d8e7f9500ba2aac
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-03-18 20:49:38 -0300

    iniciando estrutura da pagina

--- src/pages/Swap/index.jsx
+++ src/pages/Swap/index.jsx
@@ -1,7 +1,59 @@
 import styles from "./Swap.module.scss";
 
 export default function Swap() {
-  return (
-    <div>Swap</div>
-  )
+    return (
+        <section className={styles.swap}>
+            <h1>Swap</h1>
+
+            <div className={styles.swap__conteudo}>
+                <div className={styles.swap__container}>
+                    <div className={styles.wrapper}>
+                        <div>
+                            <select>
+                                <option>Moeda 1</option>
+                                <option>Moeda 2</option>
+                                <option>Moeda 3</option>
+                            </select>
+                        </div>
+
+                        <div>
+                            <input type="number" placeholder="0" />
+                        </div>
+                    </div>
+
+                    <div className={styles.wrapper}>
+                        <div>
+                            <select>
+                                <option>Moeda 1</option>
+                                <option>Moeda 2</option>
+                                <option>Moeda 3</option>
+                            </select>
+                        </div>
+
+                        <div>
+                            <input type="number" placeholder="0" />
+                        </div>
+                    </div>
+                    <input type="range" />
+
+                    <button>Confirma</button>
+                </div>
+
+                <div className={styles.swap__conteudo__saldo}>
+                    <h2>Saldo</h2>
+                    <ul>
+                        <li>
+                            <p>Token</p> <p>0.0</p>
+                        </li>
+                        <li>
+                            <p>Token</p> <p>0.0</p>
+                        </li>
+                        <li>
+                            <p>Token</p> <p>0.0</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    );
 }
